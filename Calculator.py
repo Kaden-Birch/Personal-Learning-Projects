c5c64978788486bc1e853bbe9d5324d675176f26
kadenbirch <Kaden.d.birch@gmail.com>
2026-01-14 14:02:42 -0700

    Refined division function

--- Calculator.py
+++ Calculator.py
@@ -13,33 +13,39 @@ Q --- Quit
 function = input("Enter a selection from the menu to select that operation\n")
 
 #function selection section
+
+#Addition
 while function == "1":
     x = int(input("Enter first number to add: "))
     y = int(input("Enter second number to add: "))
     calculator_functions.add(x,y)
 
 
-
+#Subtration
 while function == "2":
     x = int(input("Enter first number to subtract: "))
     y = int(input("Enter second number to subtract: "))
     calculator_functions.subtract(x, y)
 
+#multiplication
 while function == "3":
     x = int(input("Enter first number to multiply: "))
     y = int(input("Enter second number to multiply: "))
     calculator_functions.multiply(x, y)
 
+#division
 while function == "4":
     x = int(input("Enter first number to divide: "))
     y = int(input("Enter second number to divide: "))
     calculator_functions.divide(x, y)
 
+#exponent function
 while function == "5":
     x = int(input("Enter base value: "))
     y = int(input("Enter exponential value "))
     calculator_functions.exponent(x, y)
 
+#root function
 while function == "6":
     x = int(input("Enter base value: "))
     y = int(input("Enter root value "))
--- calculator_functions.py
+++ calculator_functions.py
@@ -1,29 +1,38 @@
-#add function
+#Addition function
 def add(x, y):
     answer = x + y
     print(f"{x} + {y} = {answer}")
     return
 
+#Subtraction function
 def subtract(x, y):
     answer = x - y
     print(f"{x} - {y} = {answer}")
     return
 
+
+#Multiplication function
 def multiply(x, y):
     answer = x * y
     print(f"{x} * {y} = {answer}")
     return
 
+#Division function
 def divide(x, y):
+    while y == 0:
+        print("ERROR --- CANNOT DIVIDE BY 0!")
+        y = input("Enter a valid value to divide by\n")
     answer = x / y
     print(f"{x} / {y} = {answer}")
     return
 
+#Exponent function
 def exponent(x, y):
     answer = x ** y
     print(f"{x} ^ {y} = {answer}")
     return
 
+#Root function
 def root(x, y):
     math_root = 1/y
     answer = x ** math_root
